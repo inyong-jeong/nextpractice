4b62f0372f328bec64e83a5506ee74c25eb52170
inyong-jeong <70092159+inyong-jeong@users.noreply.github.com>
2024-02-08 17:33:24 +0900

    [git setup] : initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter({ subsets: ['latin'] })
+export const metadata: Metadata = {
+  title: '넥스트',
+  description: '넥스트 프로젝트 연습',
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
